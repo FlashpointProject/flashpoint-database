d4f523ac042037b4380aa2dafbf9963bb277a057
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-01-29 13:46:21 -0500

    Add Downloads page

---
+++ downloads/downloads.js
@@ -0,0 +1,60 @@
+let downloadRoot = '.';
+
+fetch(`${downloadRoot}/info.json`).then(r => r.json()).then(json => {
+    document.querySelector('.download-compressed-size').textContent   = formatBytes(json.compressedSize);
+    document.querySelector('.download-uncompressed-size').textContent = formatBytes(json.uncompressedSize);
+    
+    for (let table in json) {
+        if (table == 'compressedSize' || table == 'uncompressedSize') continue;
+        
+        for (let entry of json[table]) {
+            let row = document.createElement('tr'),
+                entryName = document.createElement('td'),
+                entrySize = document.createElement('td'),
+                entryPath = document.createElement('td'),
+                entryDownload = document.createElement('td'),
+                entryDownloadLink = document.createElement('a'),
+                entryHash = document.createElement('td'),
+                entryHashCopy = document.createElement('span');
+            
+            entryName.textContent = entry.name
+            entrySize.textContent = formatBytes(entry.size);
+            entryPath.textContent = entry.path;
+            
+            entryDownloadLink.textContent = entry.file;
+            entryDownloadLink.href = `${downloadRoot}/${entry.file}`;
+            entryDownload.append(entryDownloadLink);
+            
+            entryHashCopy.textContent = 'Copy';
+            entryHashCopy.className = 'common-activate';
+            entryHashCopy.addEventListener('click', () => { navigator.clipboard.writeText(entry.hash) });
+            entryHash.append(entryHashCopy);
+            
+            row.append(entryName);
+            row.append(entrySize);
+            row.append(entryPath);
+            row.append(entryDownload);
+            row.append(entryHash);
+            
+            try {
+                document.getElementById(table).append(row);
+            }
+            catch {
+                document.getElementById('other').append(row);
+            }
+        }
+    }
+});
+
+function formatBytes(bytes) {
+    const units = [' bytes', 'KB', 'MB', 'GB', 'TB'];
+    
+    let i = units.length;
+    while (--i >= 0) {
+        let unitSize = Math.pow(1024, i);
+        if (bytes >= unitSize)
+            return (Math.floor((bytes / unitSize) * 100) / 100) + units[i];
+    }
+    
+    return '0 bytes';
+}
