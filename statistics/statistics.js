c8e488feb229e71305a0ce36872b9406c1f5e8d1
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-02-14 12:01:40 -0500

    Make numbers on statistics page look nicer

--- statistics/statistics.js
+++ statistics/statistics.js
@@ -1,9 +1,9 @@
 fetch('https://db-api.unstable.life/stats').then(r => r.json()).then(json => {
-    document.querySelector('.stats-games div:last-child').textContent      = json.libraryTotals[0].count;
-    document.querySelector('.stats-animations div:last-child').textContent = json.libraryTotals[1].count;
+    document.querySelector('.stats-games div:last-child').textContent      = json.libraryTotals[0].count.toLocaleString();
+    document.querySelector('.stats-animations div:last-child').textContent = json.libraryTotals[1].count.toLocaleString();
     
-    document.querySelector('.stats-legacy div:last-child').textContent     = json.formatTotals[0].count;
-    document.querySelector('.stats-gamezip div:last-child').textContent    = json.formatTotals[1].count;
+    document.querySelector('.stats-legacy div:last-child').textContent     = json.formatTotals[0].count.toLocaleString();
+    document.querySelector('.stats-gamezip div:last-child').textContent    = json.formatTotals[1].count.toLocaleString();
     
     let platforms = json.platformTotals.sort((a, b) => a.count == b.count ? 0 : (a.count > b.count ? -1 : 1));
     
@@ -13,7 +13,7 @@ fetch('https://db-api.unstable.life/stats').then(r => r.json()).then(json => {
             platformValue = document.createElement('td');
         
         platformName.textContent  = platform.name;
-        platformValue.textContent = platform.count;
+        platformValue.textContent = platform.count.toLocaleString();
         
         row.append(platformName);
         row.append(platformValue);
